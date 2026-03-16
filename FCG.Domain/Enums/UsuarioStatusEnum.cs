da16d2588cc42467c4903eaf7446e56dadbca313
Carolina Oliveira <carolina.oliveira@mbrf.com>
2026-03-16 19:38:56 -0300

    feat(domain): AdicionaUsuarioEntity com Role e StatusEnum

---
+++ FCG.Domain/Enums/UsuarioStatusEnum.cs
@@ -0,0 +1,11 @@
+namespace FCG.Domain.Enums
+{
+    public enum UsuarioStatusEnum
+    {
+        Ativo = 0,
+        Inativo = 1,
+        Suspenso = 2,
+        Bloqueado = 3,
+        Banido = 4
+    }
+}
