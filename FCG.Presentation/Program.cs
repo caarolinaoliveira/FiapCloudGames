d3df5b8031b74aacfe744eff450017298f95782b
Carolina Oliveira <carolina.oliveira@mbrf.com>
2026-04-16 15:26:10 -0300

    feat: incluir a validaçaõ automática do FluentValidation no Program.cs

--- FCG.Presentation/Program.cs
+++ FCG.Presentation/Program.cs
@@ -3,12 +3,18 @@ using FCG.Application.Configuration;
 using FCG.Infrastructure;
 using FCG.Infrastructure.Context;
 using Microsoft.EntityFrameworkCore;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using FCG.Application.Validators.Jogos;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+// Adiciona validação automática usando FluentValidation
+builder.Services.AddValidatorsFromAssemblyContaining<CriarJogoValidator>();
+builder.Services.AddFluentValidationAutoValidation();
 
 builder.Services.AddDbContext<FcgDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
